f01ba975bb1a0338307ea30056680955d0712199
akankshat4 <akankshat4@gmail.com>
2019-09-02 23:43:11 +0530

    adding constructor

--- PracticeDummyPrograms/src/arrayList/ArrayListFeatures.java
+++ PracticeDummyPrograms/src/arrayList/ArrayListFeatures.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 public class ArrayListFeatures {
 	List<Integer> intArrayList = new ArrayList<Integer>();
+	
+	public ArrayListFeatures(){
+		
+	}
+	
 	public static void main(String args[]){
 		System.out.println("Hello World");
 	}
