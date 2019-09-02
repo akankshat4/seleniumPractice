55c90f233200ae3bb007569575fe61098342edf0
akankshat4 <akankshat4@gmail.com>
2019-09-02 23:40:08 +0530

    adding new object to the class

--- PracticeDummyPrograms/src/arrayList/ArrayListFeatures.java
+++ PracticeDummyPrograms/src/arrayList/ArrayListFeatures.java
@@ -1,6 +1,10 @@
 package arrayList;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ArrayListFeatures {
+	List<Integer> intArrayList = new ArrayList<Integer>();
 	public static void main(String args[]){
 		System.out.println("Hello World");
 	}
